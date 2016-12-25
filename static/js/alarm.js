581ad8dc0484d9b91550b4dc2aae81a3757ac1a4
Spencer Hitchcock <spencerhitch@gmail.com>
2016-12-24 22:53:31 -0800

    Adding in some basic class structure to alarm.js

--- static/js/alarm.js
+++ static/js/alarm.js
@@ -1,6 +1,12 @@
 // An Alarm Clock in Javascript
 
-var Alarm = function(){
+var Alarm = function(stopwatch, alarm){
+  var self = this;
+  this.stopwatch = stopwatch;
+
+  this.stopwatch.on("click", "#stopwatch_btn", function(){
+    console.log($("#stopwatch_inp").val());
+  });
 }
 
 Alarm.prototype = {
