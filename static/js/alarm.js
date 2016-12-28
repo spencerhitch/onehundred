446d7c6dbbadfe12c32a3afffee4774543b5f2ae
Spencer Hitchcock <spencerhitch@gmail.com>
2016-12-27 21:17:43 -0800

    Just added some functionality for alarm, but still not there.

--- static/js/alarm.js
+++ static/js/alarm.js
@@ -3,9 +3,19 @@
 var Alarm = function(timer, alarm){
   var self = this;
   this.timer = timer;
+  this.alarm = alarm;
 
   this.timer.on("click", "#timer_btn", function(){
-    var time = $("#timer_inp").val();
+    var time = $("#timer_inp");
+    console.log("Time: ", time);
+    setTimeout(function(){
+       document.getElementById("alarm").play();
+       alert("Time is up!");
+    }, time * 1000);
+  });
+
+  this.alarm.on("click", "#alarm_btn", function(){
+    var time = $("#alarm_inp").val();
     setTimeout(function(){
      document.getElementById("alarm").play();
       alert("Time is up!");
