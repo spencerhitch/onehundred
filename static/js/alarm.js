7fde59f52e1a852305bd74c0efb746e1a0d0f61e
Spencer Hitchcock <spencerhitch@gmail.com>
2016-12-28 21:25:20 -0800

    Alarm functionality decent.

--- static/js/alarm.js
+++ static/js/alarm.js
@@ -16,10 +16,18 @@ var Alarm = function(timer, alarm){
 
   this.alarm.on("click", "#alarm_btn", function(){
     var time = $("#alarm_inp").val();
+    var now = new Date().toTimeString();
+    console.log('time: ', time);
+    console.log('now : ', now);
+    var alarm_time = ((parseInt(time.substring(0,2)) -  parseInt(now.substring(0,2))) 
+      * 60  + (parseInt(time.substring(3,5)) - parseInt(now.substring(3,5)))) * 60
+      - now.substring(6,8);
+      ;
+    console.log("alarm_time: ", alarm_time);
     setTimeout(function(){
      document.getElementById("alarm").play();
       alert("Time is up!");
-    }, time * 1000);
+    }, alarm_time * 1000);
   });
 }
 
